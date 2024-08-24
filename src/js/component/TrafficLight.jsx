b899fbfb8178a7b593864d90581817bb2adc80a5
Adrian <adrian.pina11@gmail.com>
2024-08-24 00:09:15 +0000

    Traffic Light

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,28 @@
+
+import React, {useState} from "react";
+import "../../styles/index.css"
+
+const TrafficLight= ()=> {
+	const [color, setColor] = useState("red")
+	const cycleColor = ()=> {
+		if (color==="red") 
+			{setColor("yellow");
+
+			}
+		else if (color==="yellow"){setColor("green");
+
+		} else {setColor("red");
+
+		}
+	}; 
+	return (
+		<div className="traffic-light">
+			<div className={`light red ${color==="red"? "glow" : ""}`} onClick={() => setColor("red")}></div>
+			<div className={`light yellow ${color==="yellow"? "glow" : ""}`} onClick={() => setColor("yellow")}></div>
+			<div className={`light green ${color==="green"? "glow" : ""}`} onClick={() => setColor("green")}></div>
+			<button onClick={cycleColor}>Cycle Light</button>
+		</div>
+		
+	 )
+}
+export default TrafficLight;
